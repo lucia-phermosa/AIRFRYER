10eb2e14000ef71292a91111414da223f9702de1
lucia-phermosa <61051941+lucia-phermosa@users.noreply.github.com>
2022-12-22 13:19:10 +0100

    Create AIRFRYER.vhd

---
+++ AIRFRYER.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity AIRFRYER is
+end AIRFRYER;
+
+architecture Behavioral of AIRFRYER is
+   signal boton_sync: std_logic;
+   signal boton_edge: std_logic;
+begin
+
+
+end Behavioral;
