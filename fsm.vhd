165d4f4ad005482e1ca7f46a0844055cf5bcf6eb
robervm <111271591+robervm@users.noreply.github.com>
2022-12-25 12:51:20 +0100

    Update fsm.vhd

--- fsm.vhd
+++ fsm.vhd
@@ -5,11 +5,18 @@ Entity fsm is
   Port( reset, clk, ok: in std_logic;
        switches: in std_logic_vector(0 to 5);
        led: out std_logic
-       TIEMPO : out STD_LOGIC_VECTOR (5 downto 0); --MAXIMO TIEMPO 63 SEGUNDOS "111111"
-       TEMPERATURA : out STD_LOGIC_VECTOR (7 downto 0); --MAXIMA TEMP 255 GRADOS "11111111");
+       TIEMPO : in STD_LOGIC_VECTOR (5 downto 0); --MAXIMO TIEMPO 63 SEGUNDOS "111111"
+       TEMPERATURA : in STD_LOGIC_VECTOR (7 downto 0); --MAXIMA TEMP 255 GRADOS "11111111");
+       display_time:out STD_LOGIC_VECTOR (5 downto 0);
+       display_temp: in STD_LOGIC_VECTOR (7 downto 0));
   end fsm;
   
-  
+  Architecture descripcion of fsm is
+       TYPE estado is
+       (comienzo, manual, automatico, ejecucion, listo);
+       signal presente:estado:=comienzo;
+       signal rescont: boolean:=false;
+       
   
  
   
