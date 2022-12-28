f74caa0819d43ed3a371c49fdf9f013af4fcb309
amayore <47856800+amayore@users.noreply.github.com>
2022-12-28 17:29:55 +0100

    Update COUNTER.vhd

--- COUNTER.vhd
+++ COUNTER.vhd
@@ -11,8 +11,8 @@ entity COUNTER is
 end COUNTER;
 
 architecture Behavioral of COUNTER is
-   signal q1: STD_LOGIC_VECTOR range'TIEMPO; -- Rango de temperatura 0-300
-   signal q2: STD_LOGIC_VECTOR range'TEMPERATURA; -- Rango de tiempo 0-30
+   signal q1: STD_LOGIC_VECTOR range'TEMPERATURA; -- Rango de temperatura 0-300
+   signal q2: STD_LOGIC_VECTOR range'TIEMPO; -- Rango de tiempo 0-30
 begin
     
     process(CLK,RESET)
@@ -22,13 +22,13 @@ begin
           q2 <= (OTHERS = > '0');
        elsif CLK'event and CLK = '1' then 
           if(TEMP_TIME(0) = '1') then
-             q1 <= q1 + "000101"; -- Incrementa la temperatura en 20 grados
+             q1 <= q1 + "00010100"; -- Incrementa la temperatura en 20 grados
           elsif (TEMP_TIME(1) = '1') then
-                q1 <= q1 - "001010"; -- Decrementa la temperatura en 10 grados
+                q1 <= q1 - "00001010"; -- Decrementa la temperatura en 10 grados
           elsif (TEMP_TIME(2) = '1') then
-                q2 <= q2 + 5; -- Incrementa el tiempo en 5 segundos
+                q2 <= q2 + "000101"; -- Incrementa el tiempo en 5 segundos
           elsif (TEMP_TIME(3) = '1') then
-                q2 <= q2 - 1; -- Decrementa el tiempo en 1 segundo
+                q2 <= q2 - "000001"; -- Decrementa el tiempo en 1 segundo
           end if;
        end if;
     end process;
