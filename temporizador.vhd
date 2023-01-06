52c0c17175d1e4f07f2287e77751f3d272e236b9
robervm <111271591+robervm@users.noreply.github.com>
2023-01-06 12:16:22 +0100

    Update temporizador.vhd

--- temporizador.vhd
+++ temporizador.vhd
@@ -11,7 +11,7 @@ Entity temporizador is
   
   Architecture descripcion of temporizador is
        TYPE estado is
-       (mode_cont, mode_done);
+       (mode_done,mode_cont,mode_over);
        signal presente:estado:=mode_done;
        signal rescont: boolean:=false;
        signal asig_time: STD_LOGIC_VECTOR (7 downto 0); -- Rango de temperatura 0-300
@@ -61,6 +61,10 @@ Entity temporizador is
             end if;
           when mode_cont=>
             if fin_cuenta then 
+                presente<=mode_over;
+            end if;
+          when mode_over=>
+            if ready='0' then 
                 presente<=mode_done;
             end if;
         end case;
@@ -72,13 +76,17 @@ Entity temporizador is
   begin
     case presente is
       when mode_done=>
-        finish<='1';
+        finish<='0';
         asig_time<=display_time;
         rescont<=true;
       when mode_cont=>
         finish<='0';
         asig_time<=display_time;
         rescont<=false;
+     when mode_over=>
+        finish<='1';
+        asig_time<=display_time;
+        rescont<=true;
    end case;
    end process salida;
      
